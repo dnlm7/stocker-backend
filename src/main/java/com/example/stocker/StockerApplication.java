d91f08acfc53fdfc9074b5b2af9465667563ca32
Martin Amaro <martingadpacks@gmail.com>
2025-06-06 22:15:35 -0500

    test: configurate to deploy on Railway

--- src/main/java/com/example/stocker/StockerApplication.java
+++ src/main/java/com/example/stocker/StockerApplication.java
@@ -1,5 +1,8 @@
 package com.example.stocker;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +10,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StockerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StockerApplication.class, args);
-	}
+		SpringApplication app = new SpringApplication(StockerApplication.class);
 
+		// Detectar puerto desde variables de entorno o usar valor por defecto
+		String port = System.getenv("PORT");
+
+		// Usar valor del sistema o fallback al .env cargado por Spring
+		if (port != null && !port.isEmpty()) {
+			Map<String, Object> props = new HashMap<>();
+			props.put("server.port", port);
+			app.setDefaultProperties(props);
+		}
+
+		app.run(args);
+	}
 }
+
